3127b90e0b91c7421b727a64938391087a6a18f0
Ryan Kennedy <ryaninvents@users.noreply.github.com>
2019-09-04 12:21:28 -0400

    fix: ensure scroll carousel is centered

--- src/scripts/sections/featured-collection.js
+++ src/scripts/sections/featured-collection.js
@@ -2,8 +2,19 @@ import {quickViewAll} from './quick-view';
 
 import '../../styles/sections/featured-collection.scss';
 
+function centerScrollCarousel(carousel) {
+  // HACK: When you apply an `element.scrollLeft` value to a scroll-snap element,
+  // the browser will "round" that value to the nearest page.
+  carousel.scrollLeft = carousel.scrollLeft;
+}
+
 function featuredCollection(baseElement) {
+  const carousel = baseElement.querySelector('[data-element="carousel"]');
   quickViewAll(baseElement);
+  centerScrollCarousel(carousel);
+  window.addEventListener('resize', () => {
+    centerScrollCarousel(carousel);
+  });
 }
 
 [...document.querySelectorAll('[data-section-type="featured-collection"]')]
